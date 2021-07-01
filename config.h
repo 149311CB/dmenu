2859a4dd65cadffb1d9ec8806574d2461156f855
149311CB <an.nickhcm2000@gmail.com>
2021-07-01 18:35:16 +0700

    gruvbox

--- config.h
+++ config.h
@@ -5,15 +5,15 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"Iosevka Nerd Font:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bd93f9", "#282a36" },
-	[SchemeSel] = { "#eeeeee", "#44475a" },
-	[SchemeSelHighlight] = { "#ffc978", "#44475a" },
-	[SchemeNormHighlight] = { "#ffc978", "#282a36" },
+	[SchemeNorm] = { "#d4be98", "#282828" },
+	[SchemeSel] = { "#d4be98", "#5a524c" },
+	[SchemeSelHighlight] = { "#d4be98", "#5a524c" },
+	[SchemeNormHighlight] = { "#ffc978", "#282828" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
