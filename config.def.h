29d08fd6407afa274e5e9478c7a3e97afe07f0db
149311CB <an.nickhcm2000@gmail.com>
2021-08-04 05:02:19 +0700

    github theme

--- config.def.h
+++ config.def.h
@@ -5,15 +5,15 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"Iosevka Nerd Font:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#d4be98", "#282828" },
-	[SchemeSel] = { "#d4be98", "#45403d" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
+	[SchemeNorm] = { "#d1d5da", "#22272e" },
+	[SchemeSel] = { "#d1d5da", "#636e7b" },
+	[SchemeSelHighlight] = { "#6cb6ff", "#636e7b" },
+	[SchemeNormHighlight] = { "#6cb6ff", "#22272e" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
--- config.h
+++ config.h
@@ -10,10 +10,10 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#d4be98", "#282828" },
-	[SchemeSel] = { "#d4be98", "#5a524c" },
-	[SchemeSelHighlight] = { "#d4be98", "#5a524c" },
-	[SchemeNormHighlight] = { "#ffc978", "#282828" },
+	[SchemeNorm] = { "#d1d5da", "#22272e" },
+	[SchemeSel] = { "#d1d5da", "#636e7b" },
+	[SchemeSelHighlight] = { "#6cb6ff", "#636e7b" },
+	[SchemeNormHighlight] = { "#6cb6ff", "#22272e" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
